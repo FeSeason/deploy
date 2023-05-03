8d2478f2951ade450baf2b119204027f5f7675e9
Season <season@macbook.local>
2023-05-03 20:26:04 +0800

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // console.log(5555)
+  console.log(5555)
   return (
     <div className="App">
       <header className="App-header">
