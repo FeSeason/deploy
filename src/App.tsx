a4b296a3c2e28a795de9a53d9e21225aa9d471f5
Season <season@macbook.local>
2023-05-12 18:25:26 +0800

    feat: modify txt

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React1111
         </a>
       </header>
     </div>
